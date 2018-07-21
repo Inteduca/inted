4b9da3b5bb7efc46fb801ec5cea2e3820ce1f70a
Jorge <inted00@gmail.com>
2018-07-21 15:09:45 +0200

    bububu

--- usuario/views.py
+++ usuario/views.py
@@ -5,5 +5,5 @@ def usuario(request, room_name):
     if request.user.is_authenticated:
         username=request.user.username
     room_name=room_name
-    context = {'room_name': room_name}
+    context = {'room_name': room_name, 'username': username}
     return render(request, "usuario.html", context)
