96ed7afce5858f70c31c22417f44ddf1a2a33ecc
Jorge <inted00@gmail.com>
2018-07-22 13:22:53 +0200

    nibi

--- usuario/views.py
+++ usuario/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from usuario.models import Comentarios
+from django.http import Http404
 
 def usuario(request, room_name):
     username = None
     score_text = None
     horario_text = None
     observaciones_text = None
+    room_name = room_name
     if request.user.is_authenticated:
         username=request.user.username
-    room_name=room_name
+    if username!=room_name and request.user.is_staff is False:
+        raise Http404("No estás identificado como el usuario de esta cuenta")
+
     if request.method=="GET":
         try:
-            score_text = Comentarios.objects.values_list('score_text', flat=True).get(username=username)
-            horario_text = Comentarios.objects.values_list('horario_text', flat=True).get(username=username)
-            observaciones_text = Comentarios.objects.values_list('observaciones_text', flat=True).get(username=username)
+            score_text = Comentarios.objects.values_list('score_text', flat=True).get(username=room_name)
+            horario_text = Comentarios.objects.values_list('horario_text', flat=True).get(username=room_name)
+            observaciones_text = Comentarios.objects.values_list('observaciones_text', flat=True).get(username=room_name)
         except:
             score_text = "Vaya, parece que ha habido un error. Por favor, háznoslo saber contactando con nosotros por correo o móvil."
             horario_text = "Vaya, parece que ha habido un error. Por favor, háznoslo saber contactando con nosotros por correo o móvil."
