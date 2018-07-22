2412299c437c1dc103d049e4a9e53a208961d0c3
Jorge <inted00@gmail.com>
2018-07-22 12:02:17 +0200

    primero

--- principal/views.py
+++ principal/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
-# en esta app voy a poner settings, pagina principal y cosillas que aparezcan
-# continuamente en la pantalla a pesar de estar en otras apps
-
-
 def hola(request):
 
     return render(request, 'hola.html')
@@ -15,4 +11,4 @@ def contacta(request):
 
 def funciona(request):
 
-    return render
+    return render(request, 'funciona.html')
--- usuario/views.py
+++ usuario/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from usuario.models import Comentarios
 
 def usuario(request, room_name):
     username = None
+    score_text = None
+    horario_text = None
+    observaciones_text = None
     if request.user.is_authenticated:
         username=request.user.username
     room_name=room_name
-    context = {'room_name': room_name, 'username': username}
+    if request.method=="GET":
+        try:
+            score_text = Comentarios.objects.values_list('score_text', flat=True).get(username=username)
+            horario_text = Comentarios.objects.values_list('horario_text', flat=True).get(username=username)
+            observaciones_text = Comentarios.objects.values_list('observaciones_text', flat=True).get(username=username)
+        except:
+            score_text = "Vaya, parece que ha habido un error. Por favor, háznoslo saber contactando con nosotros por correo o móvil."
+            horario_text = "Vaya, parece que ha habido un error. Por favor, háznoslo saber contactando con nosotros por correo o móvil."
+            observaciones_text = "Vaya, parece que ha habido un error. Por favor, háznoslo saber contactando con nosotros por correo o móvil."
+
+    context = {'room_name': room_name, 'username': username, 'horario_text':horario_text, 'score_text':score_text, 'observaciones_text':observaciones_text}
     return render(request, "usuario.html", context)
