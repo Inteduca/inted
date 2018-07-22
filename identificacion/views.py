e8df26456c1c11de457c80f3da13e72dab1cd092
Jorge <inted00@gmail.com>
2018-07-22 12:27:09 +0200

    nub

--- identificacion/views.py
+++ identificacion/views.py
@@ -25,28 +25,32 @@ def registrar(request):
     if request.method=='POST':
         username=request.POST.get('name')
         password=request.POST.get('password')
+        password2=request.POST.get('password2')
         email=request.POST.get('email')
-        try:
-            user=User.objects.create_user(username=username, password=password, email=email)
-        except:
-            raise Http404("Nombre cogido")
-        try:
-            user.save()
-        except:
-            raise Http404("Error interno")
-        try:
-            d=Comentarios(username=username)
-            d.save()
-        except:
-            raise Http404("error comentarios")
-        try:
-            user2=authenticate(request, username=username, password=password)
-            if user2 is not None:
-                login(request, user2)
-                return HttpResponseRedirect('/usuario/' + username)
-            else:
+        if password!=password2:
+            Http404("Las contraseñas no coinciden")
+        else:
+            try:
+                user=User.objects.create_user(username=username, password=password, email=email)
+            except:
+                raise Http404("Nombre cogido")
+            try:
+                user.save()
+            except:
+                raise Http404("Error interno")
+            try:
+                d=Comentarios(username=username)
+                d.save()
+            except:
+                raise Http404("error comentarios")
+            try:
+                user2=authenticate(request, username=username, password=password)
+                if user2 is not None:
+                    login(request, user2)
+                    return HttpResponseRedirect('/usuario/' + username)
+                else:
+                    raise Http404
+            except:
                 raise Http404
-        except:
-            raise Http404
 
     return render(request, 'register.html')
