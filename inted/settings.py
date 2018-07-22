9e6820d8512e6f165373e1e518135a612c95ae1a
Jorge <inted00@gmail.com>
2018-07-22 13:44:08 +0200

    nb

--- inted/settings.py
+++ inted/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!uv@6el83x+hscoa8jx*1pbavzjvo#9vc+s0r+z!bdj9jv6mp6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "inted.herokuapp.com", "0.0.0.0"]
 
